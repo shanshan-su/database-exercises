9f72680f956b905c4c279a8ab3bffa82439a8d90
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-05-18 12:31:56 -0500

    feat: tables exercises completed

---
+++ albums_migration.sql
@@ -0,0 +1,23 @@
+-- use codeup test db
+USE codeup_test_db;
+
+-- Write some SQL to drop a table named albums if it exists.
+DROP TABLE IF EXISTS albums;
+
+-- Create an albums table with the following columns:
+-- id — auto-incrementing unsigned integer primary key
+-- artist — string for storing the recording artist name
+-- name — string for storing a record name
+-- release_date — integer representing year record was released
+-- sales — floating point value for number of records sold (in millions)
+-- genre — string for storing the record's genre(s)
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100),
+    name VARCHAR(100) NOT NULL,
+    release_date INT UNSIGNED NOT NULL,
+    sales DOUBLE UNSIGNED,
+    genre VARCHAR(150),
+    PRIMARY KEY (id)
+);
+
