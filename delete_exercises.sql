712c6d4a2303a13d1833de48d6bd978e197033fb
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-05-19 09:32:06 -0500

    feat: Basic Statements exercises part 4 completed

---
+++ delete_exercises.sql
@@ -0,0 +1,23 @@
+USE codeup_test_db;
+
+-- Write SELECT statements for:
+-- Albums released after 1991
+SELECT * FROM albums
+WHERE release_date > 1991;
+
+DELETE FROM albums
+WHERE release_date > 1991;
+
+-- Albums with the genre 'disco'
+SELECT * FROM albums
+WHERE genre >= 'disco';
+
+DELETE FROM albums
+WHERE genre >= 'disco';
+
+-- Albums by 'Whitney Houston' (...or maybe an artist of your choice)
+SELECT * FROM albums
+WHERE artist = 'Whitney Houston';
+
+DELETE FROM albums
+WHERE artist = 'Whitney Houston';
