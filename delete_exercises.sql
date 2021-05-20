0bf7b179144c882487ef511964d526d7875c1c1f
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-05-20 09:44:57 -0500

    feat: added caption

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -2,13 +2,16 @@ USE codeup_test_db;
 
 -- Write SELECT statements for:
 -- Albums released after 1991
+SELECT 'Albums released after 1991' AS 'Info';
 SELECT * FROM albums
 WHERE release_date > 1991;
 
+SELECT 'Delete albums released after 1991' AS 'Info';
 DELETE FROM albums
 WHERE release_date > 1991;
 
 -- Albums with the genre 'disco'
+SELECT 'Albums with the genre ''disco''' AS 'Info';
 SELECT * FROM albums
 WHERE genre = 'disco';
 
